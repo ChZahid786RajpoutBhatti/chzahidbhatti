b896c0bfa982608b741f62aae85d7803dd3989b2
ChZahid786RajpoutBhatti <chzahidrajpoutbhatti@gmail.com>
2023-08-27 19:35:17 +0500

    first project

---
+++ practise_8/index.js
@@ -0,0 +1,113 @@
+//console.log("zahid bhatti");
+// Question no 1
+// const url="https://reqres.in/api/users?page=1";
+// const url1="https://reqres.in/api/users?page=1";
+// const url2="D:\New folder\webdev\js\practice6\aa.text";
+
+//q1
+// const asyfun= async (url)=>{
+// const res= await fetch(url);
+// console.log(await res.json());
+// }
+// console.log(asyfun(url));
+
+// q2
+//  let response=[];
+//  const fun1=async(u,u1)=>{
+//     const res1=await fetch(u)
+//     const res2=await fetch(u1)
+//     response=[res1,res2]
+//     console.log(await res1.json())
+//     console.log(await res2.json())
+//   console.log( response)
+// }
+// console.log(fun1(url,url1))
+
+// q3
+// const fun2=async(url)=>{
+//     try {
+        
+//         const res3=await fetch(url);
+//         const res4=await res3.json();
+//         return res4;
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
+// const fun3=async(res4)=>{
+//     try {
+//         const res5= JSON.stringify(res4);
+        
+//         return res5;
+        
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// const fun4=async()=>{
+//     try {
+//        const res6= await fetchData(url)
+//        const res7=await fun3(res6)
+//         console.log("function calling",res7)
+//         //console.log(await this.fun3(this.res4.json()))
+        
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// fun4()
+
+// Q4
+
+// const fun2=async(url)=>{
+   
+        
+//     const res3=await fetch(url);
+//     const res4=await res3.json();
+//     return res4;
+// } 
+
+// const fun3=async(res4)=>{
+
+//     const res5= JSON.stringify(res4);
+    
+//     return res5;
+    
+// } 
+// const fun4=async()=>{
+// try {
+//    const res6= await fun2(url)
+//    const res7=await fun3(res6)
+//     console.log("function calling",res7)
+   
+    
+// } catch (error) {
+//     console.log(error)
+// }
+// }
+// fun4()
+
+
+// q5
+// const asyfun= async (url2)=>{
+//     const res= await fetch(url2);
+//     console.log(await res.json());
+//     }
+//     console.log(asyfun(url2));
+
+//Question no 5
+
+// const callapi= async()=> {
+//     try{ 
+//         const res=await fetch("https://reqres.in/api/users?page=1");
+//         console.log(await res.jsonwa());
+//     }
+//     catch(error){
+//         console.log(error)
+//     }
+// }
+// console.log(callapi());
+
+
+
+console.log("Zahid bhatti");
